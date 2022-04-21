c7efc6e1b48870cf650c16755c21e240e88f4e2b
Karthik R <iamjustkarthik@gmail.com>
2022-04-21 17:25:29 +0530

    Introducing JSX

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-<h1>Hello World</h1>
-  );
+function getName()
+{
+  return user.firstName+" "+user.lastName;
+}
+
+const user={
+  firstName:"The Big",
+  lastName:"Bang Theory"
+}
+const head=(
+  <div>
+    {getName()}
+  </div>
+);
+return head;
 }
 
 function App2()
 {
-  return(
-    <h1>Hello W0rld 2</h1>
-  )
+  const style_img=
+  {
+    margin:"auto",
+    width: "10%",
+    paddingLeft:"45%",
+    paddingRight:"45%"
+  }
+  const style_h3={
+    textAlign:"center"
+  }
+  const img_link="https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMDAxOTUyMV5BMl5BanBnXkFtZTgwNzAxMzYzOTE@._V1_.jpg"
+  const title="Stranger Things";
+  const home_page=
+  <div>
+    <h3 style={style_h3}>{title}</h3>
+    <img style={style_img} src={img_link}></img>
+    <p>In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits. As they search for answers, the children unravel a series of extraordinary mysteries.
+</p>
+  </div>
+  return home_page;
 }
 
 export {App,App2} ;
