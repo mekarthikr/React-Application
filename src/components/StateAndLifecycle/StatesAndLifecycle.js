fbfa651cc2ec4f4a03f9c98cf6f6d2fea12cc13b
Karthik R <iamjustkarthik@gmail.com>
2022-04-25 17:28:23 +0530

    State in React

--- src/components/StateAndLifecycle/StatesAndLifecycle.js
+++ src/components/StateAndLifecycle/StatesAndLifecycle.js
@@ -5,20 +5,34 @@ export default class StatesAndLifecycle extends React.Component{
     constructor(){
       super()
       this.state={
-          Message:"Hola"
+          Message:"Hola",
+          count:0
       }
     }
     changeMessage(){
         this.setState({
             Message:"Thank you For Clicking"
+        },()=>{
+            console.log(this.state.Message)
+        })
+        console.log(this.state.Message)
+    }
+    incrementCount(){
+        this.setState({
+            count:this.state.count+1
         })
     }
+    containerState(){
+        return (<div>
+            <h1 className="center">Hello From Class - State And Lifecycle</h1>
+            <h3>Created by {this.props.UserName}</h3>
+            <h4>The message in state is <span>{this.state.Message}</span> </h4>
+            <button onClick={()=> this.changeMessage()}>Click</button>
+            <h4>Increment Operation <span>{this.state.count}</span></h4>
+            <button onClick={()=>this.incrementCount()}>Click to increment</button>
+            </div>)
+    }
     render(){
-      return (<div>
-        <h1 className="center">Hello From Class - State And Lifecycle</h1>
-        <h3>Created by {this.props.UserName}</h3>
-        <h4>The message in state is <span>{this.state.Message}</span> </h4>
-        <button onClick={()=> this.changeMessage()}>Click</button>
-        </div>)
+      return (this.containerState())
     }
 }
