bb4ccf4cd42cf64e50394ef4b9f7e0ba29e04af4
Karthik R <iamjustkarthik@gmail.com>
2022-04-25 14:44:39 +0530

    Changes to form

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-function getName()
-{
-  return user.firstName+" "+user.lastName;
-}
+//Class based component
 
-const user={
-  firstName:"The Big",
-  lastName:"Bang Theory"
-}
-const head=(
-  <div>
-    {getName()}
-  </div>
-);
-return head;
-}
-
-function App2()
-{
-  const style_img=
-  {
-    margin:"auto",
-    width: "10%",
-    paddingLeft:"45%",
-    paddingRight:"45%"
+export default class App extends React.Component{
+  constructor(props){
+    super(props)
   }
-  const style_h3={
-    textAlign:"center"
+  render(){
+    return <div>Hello From Class - App</div>
   }
-  const img_link="https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMDAxOTUyMV5BMl5BanBnXkFtZTgwNzAxMzYzOTE@._V1_.jpg"
-  const title="Stranger Things";
-  const home_page=
-  <div>
-    <h3 style={style_h3}>{title}</h3>
-    <img style={style_img} src={img_link}></img>
-    <p>In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits. As they search for answers, the children unravel a series of extraordinary mysteries.
-</p>
-  </div>
-  return home_page;
 }
-
-export {App,App2} ;
 
