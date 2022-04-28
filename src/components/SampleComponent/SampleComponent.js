92277693582b40d5dcfb2a635ddc76f3c30f1417
Karthik R <iamjustkarthik@gmail.com>
2022-04-28 13:25:01 +0530

    Todo List(CRUD operation) in React

--- src/components/SampleComponent/SampleComponent.js
+++ src/components/SampleComponent/SampleComponent.js
@@ -1,64 +1,92 @@
 import React from "react";
+import './SampleComponent.css'
 
-export default class Samplecomponents extends React.Component
-{
+export default class Samplecomponents extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {input:"",
-                        element:[]}
-      }
-    changeinput=(event)=>{
-            this.setState({
-                input:event.target.value
-            })
-            console.log(event.target.value)
+        this.state = {
+            input: "",
+            element: [],
+            edit: false,
+            editindex: 0
+        }
     }
-    submitValue=(event)=>{
+    changeinput = (event) => {
+        this.setState({
+            input: event.target.value
+        })
+    }
+    submitValue = (event) => {
         event.preventDefault();
-        let inputelem=this.state.input;
-        this.setState({element:[...this.state.element,inputelem]})
+        if (!this.state.edit) {
+            if (this.state.input === "") {
+                alert("Enter a input");
+                return false;
+            }
+            let inputelem = this.state.input;
+            this.setState({ element: [...this.state.element, inputelem], input: "" })
+        }
+        else {
+            if (this.state.input === "") {
+                alert("Enter a input");
+                return false;
+            }
+            let array = this.state.element;
+            array[this.state.editindex] = this.state.input;
+            this.setState({
+                element: array,
+                edit: false,
+                editindex: 0,
+                input: ""
+            })
+        }
     }
-    ListDelete=(id)=>
-    {
-        console.log(id)
-        // // console.log(element)
+    ListDelete = (id) => {
+        this.setState({
+            element: this.state.element.filter((element, index) => {
+                return id !== index
+            })
+        })
     }
-    displayFunc()
-    {
-        let array=Array.from(this.state.element)
-        const element=array.map((elem,index)=>{
+    ListEdit(id) {
+        this.setState({
+            edit: true, editindex: id, input: this.state.element[id]
+        })
+    }
+    displayFunc() {
+        let array = Array.from(this.state.element)
+        const element = array.map((elem, index) => {
             return (
-                <li key={index} >
-                    <div><p>{elem} key={index} </p><button key={index} onClick={()=>this.ListDelete(index)}>delete </button> <button >edit </button></div>                    
+                <li className="display-inline" key={index} >
+                    <div><p>{this.state.element[index]}</p><button className="button-delete" key={index} onClick={() => this.ListDelete(index)}>delete </button> <button className="button-edit"  onClick={() => this.ListEdit(index)}>edit </button></div>
                 </li>
-                
+
             )
         })
         return (<div>{element}</div>)
     }
-    displayList(props)
-    {
-        return(<div>{props.value}</div>)
+    displayList(props) {
+        return (<div>{props.value}</div>)
     }
-    inputFunction()
-    {
-        return(
+    inputFunction() {
+        return (
 
-            <div>
-                <form>
+            <div className="container">
+                <h1>ToDo List</h1>
+                <form className="form-container">
                     <input value={this.state.input} onChange={this.changeinput} type="text" />
-                    <button onClick={this.submitValue}>submit</button>
+                    <button className="todo-submit" onClick={(event) => this.submitValue(event)}>SUBMIT</button>
                 </form>
-                <div>
-                {this.displayFunc()}
+                <div className="todo-list">
+                    {this.displayFunc()}
                 </div>
             </div>
         );
     }
-      render() {
+    render() {
         return (
-        this.inputFunction()
+            this.inputFunction()
 
         );
-      }
+    }
 }
