3e61c729120946e6d67a24369d2787e32461ab8b
Karthik R <iamjustkarthik@gmail.com>
2022-05-06 14:00:25 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routers from './routers/routers';
-// import fragments from './components/fragments/fragments';
-import Refs from './components/refs/refs'
-import ParentRefs from './components/refs/ParentRefs';
 // import { UserProvider } from './components/context/userContext';
 // import Maincomponent from './components/context/maincomponent';
-import Fragments from './components/fragments/fragments';
-import Table from './components/fragments/Table';
-import Hoc from './components/higher order components/hoc';
 export default function App() {
   // const name = "karthik";
   // const message = "hola"
@@ -19,13 +13,7 @@ export default function App() {
           {/* <UserProvider value="karthik">
             <Maincomponent/>
           </UserProvider> */}
-          <Hoc value="karthik"/>
-          <Table/>
       <BrowserRouter>
-      <Fragments/>
-      <Refs/>
-      <ParentRefs/>
-      {/* <fragments/> */}
        <Routers/>
       </BrowserRouter>
     </div>
