36192a559bbca739eabd8fb702503fd2f0f9bc85
Karthik R <iamjustkarthik@gmail.com>
2022-05-06 14:00:10 +0530

    added new Route paths

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,7 +1,4 @@
 import React, { Component } from "react";
-// import Maincomponent from '../components/context/maincomponent';
-// import Classcomponent from '../components/components/Components';
-// import StatesAndLifecycle from '../components/state and lifecycle/StatesAndLifecycle';
 import Handlingevents from '../components/handling events/HandlingEvents'
 import ConditionalRendering from '../components/conditional rendering/ConditionalRendering';
 import ListAndKeys from '../components/list and keys/ListAndKeys';
@@ -10,27 +7,28 @@ import Samplecomponents from '../components/sample component/SampleComponent';
 import Liftingstates from '../components/lifting state up/LiftingStateUp'
 import { Route } from 'react-router'
 import { Routes } from 'react-router-dom';
-// import Maincomponent from "../components/context/maincomponent";
-// import { UserProvider } from '../components/context/userContext';
-// import Error from '../components/error boundries/Error';
-// import Errorboundries from '../components/error boundries/Errorboundries'
+import ParentRefs from "../components/refs/ParentRefs";
+import Refs from "../components/refs/refs";
+import Fragments from "../components/fragments/fragments";
+import Table from "../components/fragments/Table";
+import Hoc from "../components/higher order components/hoc";
 export default class Routers extends Component
 {
     render()
     {
         return(    
         <Routes>
-            {/* <Route path="home" element={<Classcomponent UserName={name} />} />
-            <Route path="state" element={<StatesAndLifecycle UserName={name} Message={message} />} /> */}
-            {/* <UserProvider value="karthik">
-            <Route path="context" element={Maincomponent}/>
-            </UserProvider> */}
             <Route path="events" element={<Handlingevents />} />
             <Route path="condition" element={<ConditionalRendering />} />
             <Route path="list" element={<ListAndKeys />} />
             <Route path="forms" element={<Forms />} />
             <Route path="todo" element={<Samplecomponents />} />
             <Route path="lifting" element={<Liftingstates />} />
+            <Route path="pare" element={<ParentRefs />} />
+            <Route path="refs" element={<Refs />} />
+            <Route path="fragments" element={<Fragments />} />
+            <Route path="table" element={<Table />} />
+            <Route path="hoc" element={<Hoc value="karthik" />} />
           </Routes>)
     }
 
