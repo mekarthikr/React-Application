75bc499b2bc515fdfa4845155ca2bc7596a74b42
Karthik R <iamjustkarthik@gmail.com>
2022-05-06 12:41:22 +0530

    Changes in React App

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routers from './routers/routers';
-import fragments from './components/fragments/fragments';
+// import fragments from './components/fragments/fragments';
 import Refs from './components/refs/refs'
 import ParentRefs from './components/refs/ParentRefs';
-import { UserProvider } from './components/context/userContext';
-import Maincomponent from './components/context/maincomponent';
+// import { UserProvider } from './components/context/userContext';
+// import Maincomponent from './components/context/maincomponent';
 import Fragments from './components/fragments/fragments';
 import Table from './components/fragments/Table';
 import Hoc from './components/higher order components/hoc';
 export default function App() {
-  const name = "karthik";
-  const message = "hola"
+  // const name = "karthik";
+  // const message = "hola"
   return (
     <div className='App'>
           {/* <UserProvider value="karthik">
--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Maincomponent from '../components/context/maincomponent';
-import Classcomponent from '../components/components/Components';
-import StatesAndLifecycle from '../components/state and lifecycle/StatesAndLifecycle';
+// import Maincomponent from '../components/context/maincomponent';
+// import Classcomponent from '../components/components/Components';
+// import StatesAndLifecycle from '../components/state and lifecycle/StatesAndLifecycle';
 import Handlingevents from '../components/handling events/HandlingEvents'
 import ConditionalRendering from '../components/conditional rendering/ConditionalRendering';
 import ListAndKeys from '../components/list and keys/ListAndKeys';
@@ -9,11 +9,11 @@ import Forms from '../components/forms/Forms';
 import Samplecomponents from '../components/sample component/SampleComponent';
 import Liftingstates from '../components/lifting state up/LiftingStateUp'
 import { Route } from 'react-router'
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 // import Maincomponent from "../components/context/maincomponent";
-import { UserProvider } from '../components/context/userContext';
-import Error from '../components/error boundries/Error';
-import Errorboundries from '../components/error boundries/Errorboundries'
+// import { UserProvider } from '../components/context/userContext';
+// import Error from '../components/error boundries/Error';
+// import Errorboundries from '../components/error boundries/Errorboundries'
 export default class Routers extends Component
 {
     render()
